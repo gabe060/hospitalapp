a6339d4bd5347c3f9a38b91c2dc23b0f0e343b7d
Gabriel <gabrielfg12345@gmail.com>
2025-06-06 14:56:14 -0300

    correção de estrutura de algumas partes

--- src/main/java/com/hospitalapp/hospitalapp/repository/HospitalRepository.java
+++ src/main/java/com/hospitalapp/hospitalapp/repository/HospitalRepository.java
@@ -2,7 +2,6 @@ package com.hospitalapp.hospitalapp.repository;
 
 import com.hospitalapp.hospitalapp.model.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
